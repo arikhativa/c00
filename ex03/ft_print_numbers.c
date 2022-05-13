1772eda08a74b302eeaf9cd343a6de1a872a1073
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-13 11:08:02 +0200

    ex03 done

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 16:23:32 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/12 17:15:36 by yrabby           ###   ########.fr       */
+/*   Created: 2022/05/13 11:03:14 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/13 11:07:25 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void	ft_print_numbers(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+	write(1, "0123456789", 10);
 }
