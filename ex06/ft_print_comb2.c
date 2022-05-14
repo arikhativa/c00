9bd6e91bfd7d8f41a4aecdde03a0d5c3311ef6cd
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-14 17:19:46 +0200

    ex06 done

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -3,26 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yrabby <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/13 11:03:14 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/13 15:27:33 by yrabby           ###   ########.fr       */
+/*   Created: 2022/05/14 17:19:32 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/14 17:19:33 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void print(char n)
+void	print(char n)
 {
-	char digit1;
-	char digit2;
+	char	digit1;
+	char	digit2;
 
 	digit1 = n % 10;
 	digit2 = (n - digit1) / 10;
-
 	digit1 += '0';
 	digit2 += '0';
-
 	write(1, &digit2, 1);
 	write(1, &digit1, 1);
 }
@@ -32,11 +30,8 @@ void	print_numbers(char a, char b)
 	print(a);
 	write(1, " ", 1);
 	print(b);
-
-	if ()
-	{
+	if (a != 98)
 		write(1, ", ", 2);
-	}
 }
 
 void	ft_print_comb2(void)
@@ -55,10 +50,4 @@ void	ft_print_comb2(void)
 		}
 		++a;
 	}
-}
-
-int	main(void)
-{
-	ft_print_comb2();
-	return 0;
 }
